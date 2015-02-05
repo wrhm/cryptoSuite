c611209bdb994858d65e662a37b60b017a37bf79
wrhm <bmatchen@gmail.com>
2015-02-05 00:57:26 -0500

    control flow, 1st 3 ciphers

--- cS.py
+++ cS.py
@@ -8,11 +8,34 @@ For now, I/O is handled via command line.
 
 from lib import *
 
+print '***************************'
+print '*                         *'
+print '* Welcome to cryptoSuite! *'
+print '*                         *'
+print '***************************\n'
+print 'Follow the prompts to (en/de)crypt your message.\n'
+
+ciphers = {'a':'atbash','c':'caesar','r':'rot13'}
+modes = {'e':'Encrypting','d':'Decrypting'}
 #Main control structure
-entry = None
-while True:#response!="":
-	entry = raw_input(">> ")
-	if len(entry)==0:
-		break
-	output = ROT13(entry)
-	print output
+while True:#
+	direction,cipher = '',''
+	cipher = parse("Pick a cipher: (a)tbash, (c)aesar, (r)ot13\n>> ", 'acr')
+	print 'Using %s.'%(ciphers[cipher])
+	if cipher=='c':
+		direction = parse("Pick mode: (e)ncrypt, (d)ecrypt\n>> ", 'ed')
+	mode = cipher+direction
+	if direction!='':
+		print '%s...'%modes[direction]
+	entry = raw_input('\n  text: ')
+	if mode[0]=='c':
+		shift = parse_int(raw_input(" shift: "))
+		if mode[1]=='e':
+			output = caesar(entry,shift)
+		else:
+			output = caesar(entry,26-shift)
+	elif mode=='a':
+		output = atbash(entry)
+	elif mode=='r':
+		output = rot13(entry)
+	print 'Result: %s\n'%output
--- lib.py
+++ lib.py
@@ -5,14 +5,52 @@ import string
 #Useful abbreviations
 auc = string.ascii_uppercase
 alc = string.ascii_lowercase
+dig = string.digits
 
-def ROT13(message):
+def parse(prompt,valid_responses):
+	while True:
+		response = raw_input(prompt)
+		if(len(response)>0):
+			f = response[0]
+			if f=='q':
+				quit()
+			if f in valid_responses:
+				return f
+
+def parse_int(s):
+	val = ""
+	for c in s:
+		if c=='-' and '-' not in val:
+			val += c
+		if c in dig:
+			val += c
+	if len(val)==0:
+		return 0
+	return int(val)
+
+def caesar(message,shift):
+	while shift<0:
+		shift+=26
 	result = ""
 	for character in message:
 		if character in auc:
-			result += auc[(auc.index(character)+13)%26]
+			result += auc[(auc.index(character)+shift)%26]
 		elif character in alc:
-			result += alc[(alc.index(character)+13)%26]
+			result += alc[(alc.index(character)+shift)%26]
+		else:
+			result += character
+	return result
+
+def rot13(message):
+	return caesar(message,13)
+
+def atbash(message):
+	result = ''
+	for character in message:
+		if character in alc:
+			result += alc[25-alc.index(character)]
+		elif character in auc:
+			result += auc[25-auc.index(character)]
 		else:
 			result += character
 	return result
